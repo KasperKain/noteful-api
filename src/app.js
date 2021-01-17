f15f4830aca5b3eacf6bb6eeeebe3b0fbf05cf0f
Patrick Dodd <kasperkain@gmail.com>
2021-01-16 20:47:56 -0500

    update

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const validateBearerToken = require('./validate-bearer-token');
+const errorHandler = require('./error-handler');
 const { NODE_ENV } = require('./config');
 
 const app = express();
@@ -10,20 +12,13 @@ const app = express();
 app.use(morgan(NODE_ENV === 'production' ? 'tiny' : 'common'));
 app.use(helmet());
 app.use(cors());
+app.use(express.json());
+app.use(validateBearerToken);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
+app.use(errorHandler);
 
 module.exports = app;
