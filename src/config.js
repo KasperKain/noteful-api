16cc2a67c0d5a4f3a624ce3a4730882c641ee148
Patrick Dodd <kasperkain@gmail.com>
2021-02-02 17:23:32 -0500

    first commit

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN,
-  DATABASE_URL: `${process.env.DATABASE_URL}?ssl=true`,
+  DATABASE_URL: `${process.env.DATABASE_URL}?ssl=false`,
 };
